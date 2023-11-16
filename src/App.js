600c0a750fc6878f0ae0371bf02518af143fbb92
mrduman <omer.duman@gmx.de>
2023-11-16 18:38:02 +0100

    Basic constructur has been created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import "./App.css";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>Appss</div>;
+  return (
+    <Router>
+      <Box width={"400px "} sx={{ width: { xl: "1488px" } }} m={"auto"}>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise:id" element={<ExerciseDetail />} />
+        </Routes>
+
+        <Footer />
+      </Box>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
+
+const Navbar = () => {
+  return (
+    <Stack>
+      <Link to={"/"}>
+        <img src={Logo} alt="Logo" />
+      </Link>
+      <Stack>
+        <Link
+          to={"/"}
+          style={{
+            textDecoration: "none",
+            color: "#3A1212",
+            borderBottom: "3px solid #FF2625",
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: "none", color: "#3A1212" }}
+        >
+          Exercises
+        </a>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Navbar;
