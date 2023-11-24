7758367ced273d088599b2d704e1496986721a05
mrduman <omer.duman@gmx.de>
2023-11-24 21:49:08 +0100

    Pagination has been implemented

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,4 +1,4 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Pagination, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,10 +1,23 @@
-import React, { useEffect } from "react";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
-import { Box, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Pagination, Stack, Typography } from "@mui/material";
 import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ setExercises, bodyPart, exercises }) => {
-  console.log("ex", exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const pageSize = 10;
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  const currentExercises = exercises.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt={"50px"} p={"20px"}>
       <Typography variant="h3" mb={"46px"}>
@@ -17,7 +30,7 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
         flexWrap={"wrap"}
         justifyContent={"center"}
       >
-        {exercises.map((exercise, index) => {
+        {currentExercises.map((exercise, index) => {
           return (
             <p key={index}>
               {<ExerciseCard key={index} exercise={exercise} />}
@@ -25,8 +38,30 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
           );
         })}
       </Stack>
+      <Stack mt={"100px"} alignItems={"center"}>
+        {exercises.length > 10 && (
+          <Pagination
+            page={currentPage}
+            count={Math.ceil(exercises.length / pageSize)}
+            onChange={paginate}
+            color="primary"
+            shape="rounded"
+            size="large"
+            defaultPage={1}
+          />
+        )}
+      </Stack>
     </Box>
   );
 };
 
 export default Exercises;
+
+/** Second solution of pagination
+   *   const indexOfLastExercises = currentPage * pageSize;
+       const indexOfFirstExercises = indexOfLastExercises - pageSize;
+       const currentExercises = exercises.slice(
+       indexOfFirstExercises,
+       indexOfLastExercises
+  );
+   */
